1dc371cf2f84c7c66a7df2cb587de04f9c249b74
FedeBrg <fede.bergagna@gmail.com>
2019-06-30 21:22:53 -0300

    Mail con codigo de evento

--- app/src/main/java/com/example/ticketastic/PaymentActivity.java
+++ app/src/main/java/com/example/ticketastic/PaymentActivity.java
@@ -1,20 +1,29 @@
 package com.example.ticketastic;
 
+import android.content.AsyncQueryHandler;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class PaymentActivity extends AppCompatActivity {
 
     Ticket t;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        context = getApplicationContext();
         getSupportActionBar().setTitle("Payment details");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -26,6 +35,32 @@ public class PaymentActivity extends AppCompatActivity {
             public void onClick(View v) {
                 DatabaseHandler dbh = new DatabaseHandler(v.getContext());
                 dbh.addTicket(t);
+
+                   new AsyncTask<Void, Void, Void>() {
+                        @Override
+                        protected Void doInBackground(Void[] objects) {
+                            String body = String.format("Thank you for your purchase! Your code is %s", t.getCode());
+
+                            try {
+                            GMailSender sender = new GMailSender("ticketasticnoreply@gmail.com", "1234abcd5678");
+                            sender.sendMail("Purchase information",
+                                    body,
+                                    "ticketasticNoReply@gmail.com",
+                                    PreferenceUtils.getUsername(context));
+                            }   catch (Exception e) {
+                                Log.e("SendMail", e.getMessage(), e);
+                            }
+                            return null;
+
+                        }
+                    }.execute();
+
+
+//                try {
+//                    Thread.sleep(1);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
                 Intent intent = new Intent(PaymentActivity.this,ConfirmationActivity.class);
                 intent.putExtra("ticket",t);
                 startActivity(intent);
